bde534a7a4bca0943cf5c90e3d0063ce1778e4d4
karenmuniz04 <147630495+karenmuniz04@users.noreply.github.com>
2025-02-20 12:45:34 -0300

    Add files via upload

--- assets/js/curtas.js
+++ assets/js/curtas.js
@@ -9,37 +9,36 @@ document.addEventListener('DOMContentLoaded', function() {
         // Define os curtas para cada oficina
         const curtas = {
             oficina_III: [
-                { title: 'Diretor: Gustavo Lima', video: './videos/oficina_3/1_Vídeo P. Gustavo Lima.mp4', image: './images/oficina_3/minuto01.jpg' },
-                { title: 'Diretor: Gustavo Lima', video: './videos/oficina_3/2_Vídeo P. Gustavo Lima.mp4', image: './images/oficina_3/minuto02.jpg' },
-                { title: 'Diretor: Janine Galvão', video: './videos/oficina_3/3_Vídeo P. Janine Galvão.mp4', image: './images/oficina_3/minuto03.jpg'},
-                { title: 'Diretor: João Eduardo', video: './videos/oficina_3/4_Vídeo P. João Eduardo.mp4', image: './images/oficina_3/minuto04.jpg'},
-                { title: 'Diretor: Luciano Reis', video: './videos/oficina_3/5_Video P. Luciano Reis.mp4', image: './images/oficina_3/minuto05.jpg'},
-                { title: 'Diretor: Luciano Reis', video: './videos/oficina_3/6_Vídeo P. Luciano Reis.mp4', image: './images/oficina_3/minuto06.jpg'},
-                { title: 'Diretor: Márcio César', video: './videos/oficina_3/7_Vídeo P. Marcio Cesar.mp4', image: './images/oficina_3/minuto07.jpg'},
-                { title: 'Diretor: Márcio César', video: './videos/oficina_3/8_Vídeo P. Marcio Cesar.mp4', image: './images/oficina_3/minuto08.jpg'},
-                { title: 'Diretora: Rosângela', video: './videos/oficina_3/9_Vídeo P. Rosângela.mp4', image: './images/oficina_3/minuto09.jpg'},
-                { title: 'Diretora: Paula Ortiz', video: './videos/oficina_3/10_Vídeo Paula Ortiz.mp4', image: './images/oficina_3/minuto10.jpg'}
+                { title: 'Diretor: Gustavo Lima', video: 'https://youtu.be/OZuzLHkIIm0'},
+                { title: 'Diretor: Gustavo Lima', video: 'https://youtu.be/0Ouxb2dJNWw?si=1EUnOqSd6k2W1WVA'},
+                { title: 'Diretor: Janine Galvão', video: 'https://youtu.be/R4JViKN0LVc?si=9sj03ulKZyqFeoQ9'},
+                { title: 'Diretor: João Eduardo', video: 'https://youtu.be/x5iW7zzl0Ok?si=-07nazQ8m75vR_CA'},
+                { title: 'Diretor: Luciano Reis', video: 'https://youtu.be/z_hFdGyDMxY?si=0_q-4N7kJ-uE_OPd'},
+                { title: 'Diretor: Luciano Reis', video: 'https://youtu.be/ASHUQdKZE-E?si=Ab9jVDh3nhOiayJ8'},
+                { title: 'Diretor: Márcio César', video: 'https://youtu.be/oIl0udKe50k?si=ydlxHFm33UgVUCMm'},
+                { title: 'Diretor: Márcio César', video: 'https://youtu.be/-FxiMLD-Jpw?si=bpvxLEtnqRFCTrny'},
+                { title: 'Diretora: Rosângela', video: 'https://youtu.be/nE7zi-SCWdk?si=iaPsSaxc1A5JUrmQ'}
             ],
             oficina_IV: [
-                { title: 'Diretor: Eudes', video: './videos/oficina_4/1_Vídeo P. Eudes.mp4', image: 'images/oficina_4/planos01.jpg' },
-                { title: 'Diretor: Gustavo Lima', video: './videos/oficina_4/2_Vídeo P. Gustavo Lima.mp4', image: 'images/oficina_4/planos02.jpg' },
-                { title: 'Diretor: João Eduardo', video: './videos/oficina_4/3_Vídeo P. João Eduardo.mp4', image: 'images/oficina_4/planos03.jpg' },
-                { title: 'Diretor: Márcio César', video: './videos/oficina_4/4_Vídeo Prof Marcio Cesar.mp4', image: 'images/oficina_4/planos04.jpg' },
-                { title: 'Diretora: Rosângela Reinaldo', video: './videos/oficina_4/5_Vídeo Prof Rosângela Reinaldo.mp4', image: 'images/oficina_4/planos05.jpg' },
-                { title: 'Diretor: Luciano Reis ', video: './videos/oficina_4/6_Vídeo Prof. Luciano Reis.mp4', image: 'images/oficina_4/planos06.jpg' },
-                { title: 'Diretora: Rosângela Reinaldo', video: './videos/oficina_4/7_Vídeo Prof. Rosângela Reinaldo.mp4', image: 'images/oficina_4/planos07.jpg' }
+                { title: 'Diretor: Eudes', video: 'https://youtu.be/QSxxydmofsw?si=88Y3cZSQ--kT3hWI'},
+                { title: 'Diretor: Gustavo Lima', video: 'https://youtu.be/TP196oTGRkI?si=eP5h9ltTWumHdWuB'},
+                { title: 'Diretor: João Eduardo', video: 'https://youtu.be/1wV6ngpHhH0?si=_xiAN0Ym5PRjun0Q'},
+                { title: 'Diretor: Márcio César', video: 'https://youtu.be/YVnNAhdhHg8?si=CVmOg8TDyhkhYzuD'},
+                { title: 'Diretora: Rosângela Reinaldo', video: 'https://youtu.be/96efKcXIWBU?si=-2slNRjvdWuRFIVQ'},
+                { title: 'Diretor: Luciano Reis ', video: 'https://youtu.be/ieIANdf7RlM?si=idfKdw60zVad8hIi'},
+                { title: 'Diretora: Rosângela Reinaldo', video: 'https://youtu.be/bN7hYWov5y0?si=_gxxExa6s92Pwll7'}
             ],
             oficina_V: [
-                { title: 'Diretor: Janine Galvão', video: './videos/oficina_5/1_Vídeo Janine Galvão.mp4', image: 'images/oficina_5/Cortes 01.jpg' },
-                { title: 'Diretor: João Eduardo', video: './videos/oficina_5/2_Vídeo João Eduardo.mp4', image: 'images/oficina_5/Cortes02.jpg' }
+                { title: 'Diretor: Eudes', video: 'https://youtu.be/hSIQuIgEjuo?si=eU-hbYcZfS57rwe_'},
+                { title: 'Diretor: Eudes', video: 'https://youtu.be/s9NI4Txe6PY?si=WIJGmezl1-Ans7sR'}
             ],
             oficina_VI: [
-                { title: '', video: './videos/oficina_6/Vídeo 1.mp4', image: './images/oficina_6/1.jpeg' },
-                { title: '', video: './videos/oficina_6/Vídeo 2.mp4', image: './images/oficina_6/2.jpeg' },
-                { title: '', video: './videos/oficina_6/Vídeo 3.mp4', image: './images/oficina_6/3.jpeg' },
-                { title: '', video: './videos/oficina_6/Vídeo 4.mp4', image: './images/oficina_6/4.jpeg' },
-                { title: '', video: './videos/oficina_6/Vídeo 5.mp4', image: './images/oficina_6/5.jpeg' },
-                { title: '', video: './videos/oficina_6/Vídeo 6.mp4', image: './images/oficina_6/6.jpeg' }
+                { title: '', video: 'https://youtu.be/bfvaj-CfpzA?si=pq4SxNY8SZj5_10m'},
+                { title: '', video: 'https://youtu.be/Sj98ha19F-c?si=03rfN9K7CLDxdv3b'},
+                { title: '', video: 'https://youtu.be/QRT9HRY5G3Q?si=RRfJDwPkOkYp2FV2'},
+                { title: '', video: 'https://youtu.be/y6hRk6M8JcA?si=6MUdstGBfgVYxTER'},
+                { title: '', video: 'https://youtu.be/KL1DVsH22ck?si=OhflWBOrDcejUTKC'},
+                { title: '', video: 'https://youtu.be/qIPM4pYOH-U?si=aG_zPbFed8YB8vpy'}
             ],
             oficina_VII: [
                 { title: '', video: './videos/oficina_7/1.MOV', image: './images/oficina_7/1.png' },
@@ -73,24 +72,26 @@ document.addEventListener('DOMContentLoaded', function() {
                 const curtaItem = document.createElement('div');
                 curtaItem.classList.add('curta-item');
 
-                const img = document.createElement('img');
-                img.src = curta.image; // Imagem do curta
-                img.alt = curta.title;
-
-                const figcaption = document.createElement('figcaption');
-
                 const title = document.createElement('h2');
                 title.textContent = curta.title;
 
-                const videoLink = document.createElement('a');
-                videoLink.href = curta.video;
-                videoLink.textContent = 'Assista aqui!';
+                let videoId = curta.video.split('/').pop(); 
+                if (videoId.includes('?')) {
+                    videoId = videoId.split('?')[0]; 
+                }
+                const embedUrl = `https://www.youtube.com/embed/${videoId}`;
 
-                figcaption.appendChild(title);
-                figcaption.appendChild(videoLink);
+                const iframe = document.createElement('iframe');
+                iframe.src = embedUrl;
+                iframe.width = '760';
+                iframe.height = '315';
+                iframe.frameBorder = '0';
+                iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+                iframe.allowFullscreen = true;
+                iframe.title = 'YouTube video player';
 
-                curtaItem.appendChild(img);
-                curtaItem.appendChild(figcaption);
+                curtaItem.appendChild(title);
+                curtaItem.appendChild(iframe);
 
                 curtasContainer.appendChild(curtaItem);
             });
